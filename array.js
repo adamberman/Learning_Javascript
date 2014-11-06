1d76376ce1372a2a59735277811160146ec94547
App Academy Student <student@appacademy.io>
2014-11-06 11:48:18 -0800

    make functions twoSum and myTranspose

--- array.js
+++ array.js
@@ -9,3 +9,28 @@ Array.prototype.uniq = function(){
   return myAnswerArray;
 };
 
+Array.prototype.twoSum = function() {
+  var myAnswerArray = [];
+  var len = this.length;
+  for(var i = 0; i < (len - 1); i++) {
+    for(var j = (i + 1); j < len; j++) {
+      if(this[i] + this[j] === 0) {
+        myAnswerArray.push([i, j]);
+      }
+    }
+  }
+  return myAnswerArray;
+};
+
+var myTranspose = function(arr){
+  var myAnswerArray = [];
+  var len = arr.length;
+  for(var i = 0; i < len; i++) {
+    myAnswerArray.push([]);
+    for(var j = 0; j < len; j++) {
+      myAnswerArray[i][j] = arr[j][i];
+    }
+  }
+  return myAnswerArray;
+};
+
