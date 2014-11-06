ce0de0042bc3a64c86842402dc58b04267ca8ae4
App Academy Student <student@appacademy.io>
2014-11-06 14:26:42 -0800

    more basic javascript functions

--- array.js
+++ array.js
@@ -34,3 +34,69 @@ var myTranspose = function(arr){
   return myAnswerArray;
 };
 
+var myDoubler = function(arr){
+  var myAnswerArray = [];
+  var len = arr.length;
+  for(var i = 0; i < len; i++){
+    myAnswerArray.push(arr[i] * 2);
+  }
+  return myAnswerArray;
+};
+
+Array.prototype.myEach = function(callback){
+  var len = this.length;
+  for(var i = 0; i < len; i++) {
+    callback(this[i]);
+  }
+  return this;
+};
+
+Array.prototype.myMap = function(callback){
+  var myAnswerArray = [];
+  this.myEach(function (el) {
+   myAnswerArray.push(callback(el));
+  });
+  return myAnswerArray;
+};
+
+Array.prototype.myInject = function(callback) {
+  var myAnswer = this.shift(1);
+  this.myEach(function (el) {
+    myAnswer = callback(myAnswer, el);
+  });
+  return myAnswer;
+};
+
+var bubbleSort = function(arr) {
+  var sorted = false;
+  while (sorted === false) {
+    sorted = true;
+    var len = arr.length;
+    for(var i = 0; i < (len - 1); i++){
+      if (arr[i] > arr[i + 1]){
+        sorted = false;
+        var placeholder = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = placeholder;
+      }
+    }
+  }
+  return arr;
+};
+
+var subStrings = function(str) {
+  var result = [];
+  var arr = str.split("");
+  var len = arr.length;
+  for(var i = 0; i < len; i++){
+    for(var j = i; j < len; j++){
+      var subarr = [];
+      for(var k = i; k <= j; k++){
+        subarr.push(arr[k]);
+      }
+      var substr = subarr.join("");
+      result.push(substr);
+    }
+  }
+  return result.uniq();
+};
