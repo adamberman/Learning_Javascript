86f48bf165423bdd16381658a6b054a9584e9eaa
App Academy Student <student@appacademy.io>
2014-11-13 11:57:55 -0800

    create click listener, active tags, and transitions

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,7 +1,37 @@
-$.Tabs = function (el) { ... };
+(function(){
+   if(typeof "undefined"){
+     window.$ = {};
+   }
+})
+
+$.Tabs = function (el) {
+  this.$el = $(el);
+  this.$contentTabs = $(this.$el.data('content-tabs'));
+  this.$activeTab = $(this.$contentTabs.find('.active'));
+  this.clickTab();
+  
+};
 
 $.fn.tabs = function () {
   return this.each(function () {
     new $.Tabs(this);
   });
 };
+
+$.Tabs.prototype.clickTab = function() {
+  var that = this
+  this.$el.on("click", 'li', function(event){
+    event.preventDefault();
+    that.$activeTab.removeClass('active');
+    var $currentDiv = $(that.$activeTab.filter('div'));
+    $currentDiv.addClass('transitioning');
+    var $currentTab = $(event.currentTarget);
+    $currentTab.addClass('active');
+    
+    var $id = $currentTab.find('a').attr('href');
+    var $div = $(that.$el.find($id));
+    $div.addClass('active');
+    that.$activeTab = $(that.$contentTabs.find('.active'));
+  } )
+  
+}
